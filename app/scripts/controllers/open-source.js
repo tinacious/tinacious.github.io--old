3af265f8885e82486680fbe705f240a4248a4a02
Tina Holly (TinaciousDesign.com) <tinacious.design@gmail.com>
2014-02-20 22:06:03 -0500

    improved github repos section

--- app/scripts/controllers/open-source.js
+++ app/scripts/controllers/open-source.js
@@ -2,9 +2,15 @@
 
 angular.module('tinaciousApp')
   .controller('OpenSourceCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.repos = [
+      'goalProgress',
+      'angular-goalProgress',
+      'angular-picturefill',
+      'tinaciousFluidVid',
+      'salary-hourly-calculator',
+      'angular-tip-calc-bill-split',
+      'textShrinkGrow',
+      'shrinkList',
+      'itizens-connected-theme'
     ];
   });
